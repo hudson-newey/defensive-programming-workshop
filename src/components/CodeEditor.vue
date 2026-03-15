cfb2764ae78ab363a12ab475c6e94f40c5367c32
hudson-newey <hudsonnewey@gmail.com>
2026-03-15 20:14:14 +1000

    Add tio submissions

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,8 +1,10 @@
-<script setup>
+<script setup lang="ts">
 import { ref } from 'vue'
 import { CodeEditor } from 'monaco-editor-vue3'
+import { runCode } from '../utils/runCode'
+import type { Ref } from 'vue';
 
-defineProps({
+const { code } = defineProps({
   code: {
     type: String,
     required: true
@@ -14,23 +16,57 @@ const editorOptions = {
   minimap: { enabled: false },
   automaticLayout: true
 }
+
+const execOutput: Ref<string | null> = ref(null)
+const isSubmitting = ref(false)
+
+function checkCode() {
+  isSubmitting.value = true
+  runCode(code, '42aa\n')
+    .then((response) => {
+      execOutput.value = response.output
+    })
+    .catch((error) => {
+      execOutput.value = `Error: ${error.message}`
+    })
+    .finally(() => {
+      isSubmitting.value = false
+    })
+}
 </script>
 
 <template>
-  <CodeEditor
-    :value="code.trimStart()"
-    language="java"
-    theme="vs-dark"
-    :options="editorOptions"
-  />
+  <CodeEditor :value="code.trimStart()" language="java" theme="vs-dark" :options="editorOptions" />
+
+  <div class="mt-2">
+    <button class="button is-primary is-small" @click="checkCode" :disabled="isSubmitting">
+      {{ isSubmitting ? 'Submitting...' : 'Check' }}
+    </button>
+    <button class="button is-small ml-2">Reset</button>
+  </div>
+
+  <div class="modal" :class="{ 'is-active': execOutput !== null }">
+    <div class="modal-background"></div>
+
+    <div class="modal-card">
+      <section class="modal-card-body">
+        <p class="modal-card-title">Output</p>
+        <pre>{{ execOutput }}</pre>
+      </section>
+
+      <footer class="modal-card-foot p-2 pl-3">
+        <button class="button is-primary" @click="execOutput = null">Close</button>
+      </footer>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-  .monaco-code-editor {
-    flex: 1 1 0;
-    min-height: 0;
+.monaco-code-editor {
+  flex: 1 1 0;
+  min-height: 0;
 
-    border-radius: 0.5rem;
-    contain: content;
-  }
+  border-radius: 0.5rem;
+  contain: content;
+}
 </style>
---
+++ src/utils/runCode.ts
@@ -0,0 +1,46 @@
+import zlib from 'zlib';
+
+interface TioResponse {
+    output: string;
+}
+
+export async function runCode(code: string, inputs: string): Promise<TioResponse> {
+    return await run(code, 'java-openjdk', {
+        inputs: inputs,
+    })
+}
+
+const API_URL = 'https://tio.run/cgi-bin/run/api/';
+
+/**
+ * @param {string} code
+ * @param {string} language
+ * @param {Record<string, unknown>} options
+ */
+async function run(code: string, language: string, options: Record<string, unknown> = {}) {
+    let files: any = { ".code.tio": String(code) };
+    let variables: any = { "lang": language };
+
+    if (options.input != null) files[".input.tio"] = String(options.input);
+    if (options.args != null) variables["args"] = options.args;
+    if (options.flags != null) variables["TIO_CFLAGS"] = options.flags;
+    let plain = '';
+    for (const k in variables) {
+        plain += `V${k}\0`;
+        let v = variables[k];
+        if (!Array.isArray(v)) v = [v];
+        plain += `${v.length}\0`;
+        for (const x of v) plain += `${x}\0`;
+    }
+    for (const k in files) {
+        let v = files[k];
+        plain += `F${k}\0${Buffer.byteLength(v)}\0${v}`;
+    }
+    plain += 'R';
+    const buf = zlib.deflateRawSync(plain);
+    const request = await fetch(API_URL, { method: 'POST', body: buf });
+    const body = await request.text();
+    const splitter = body.substring(0, 16);
+    const output = body.replaceAll(splitter, '\n');
+    return { output };
+}
